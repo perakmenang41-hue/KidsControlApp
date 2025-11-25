41c9960b3d919b20f10e2fe6c6f16173ed158403
Irfan Fitri <lilskert5@gmail.com>
2025-11-26 03:41:10 +0800

    Update firebase.js to use env variable for Firebase

--- firebase.js
+++ firebase.js
@@ -1,8 +1,19 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccountKey.json'); // <- your file
+// firebase.js
+const admin = require("firebase-admin");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+// Initialize Firebase Admin only once
+if (!admin.apps.length) {
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error(
+      "FIREBASE_SERVICE_ACCOUNT environment variable not set"
+    );
+  }
+
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
